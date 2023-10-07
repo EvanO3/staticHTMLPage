aa8efd299e295430a935cccc95677343e68df939
EvanO3 <Evan.oni@hotmail.com>
2023-10-06 21:11:44 -0400

    added search bar functionality

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/** This function selects the input and the search elemnent puts a listener
+ * so when clicked the form should appear and when not clicked the form should
+ * have no style
+ */
 const SearchLink = document.getElementById('searchLink');
 const searchBar = document.getElementById('SearchBar')
 
